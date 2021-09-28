5eb79e9a99af4d16c64fdc4c8cc59b4799831ffa
Brian Leip <brianleip@gmail.com>
2021-09-28 15:21:46 -0700

    Updated boidSpawner to handle jobs/non-jobs spawning

--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class BoidSpawner : MonoBehaviour {
     public static Boid[] population;
     [SerializeField] private int totalBoids = 5000;
+    [Tooltip("Must check or uncheck this before pressing Play in Unity Editor")]
+    [SerializeField] private bool useJobSystem = false;
     [SerializeField] private Transform spawnLocation;
     [SerializeField] private GameObject fishPrefab;
     private float boundaryRadius = 10;
@@ -17,6 +19,14 @@ public class BoidSpawner : MonoBehaviour {
         else
             boundaryRadius = spawnLocation.localScale.x/2;
         
+        if (useJobSystem) {
+            SpawnBoidsJobs(totalBoids);
+        } else {
+            SpawnBoids(totalBoids);
+        }
+    }
+
+    public void SpawnBoids(int number) {
         population = new Boid[totalBoids];
         Boid newBoid;
         for (int i=0; i<totalBoids; i++) {
@@ -26,5 +36,9 @@ public class BoidSpawner : MonoBehaviour {
             population.Append(newBoid);
         }
         Debug.Log("Total boids: " + population.Length);
+    }
+
+    public void SpawnBoidsJobs(int number) {
+        Debug.Log("Job system ain't ready yet!");
     }
 }

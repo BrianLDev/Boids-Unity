4fb4cce9503618af6fdb2cf3e86ba6e3cdc84448
Brian Leip <brianleip@gmail.com>
2021-10-20 22:20:19 -0700

    Added UI controls connected to boid settings

--- Assets/Scripts/BoidSettings.cs
+++ Assets/Scripts/BoidSettings.cs
@@ -7,7 +7,7 @@ public class BoidSettings : ScriptableObject {
     public int totalBoids = 250;
     [Tooltip("Must check or uncheck this before pressing Play in Unity Editor")]
     public bool spawnUsingJobSystem = false;
-    [Range(0, 5f)]
+    [Range(0.1f, 5f)]
     public float mass = 1;
     [Range(0, 8)]
     public float speed = 3;
@@ -15,13 +15,35 @@ public class BoidSettings : ScriptableObject {
     public float maxForce = 0.1f;
     [Range(0, 2f)]
     public float perceptionRange = 0.5f;
-    [Range(0, 2f)]
+    [Range(0, 1f)]
     public float separationStrength = 0.2f;
-    [Range(0, 2f)]
+    [Range(0, 1f)]
     public float alignmentStrength = 1f;
-    [Range(0, 2f)]
+    [Range(0, 1f)]
     public float cohesionStrength = 1f;
     public bool moveFwd = true;
     public bool boundsOn = true;
     public bool drawDebugLines = false;
+
+    public void ChangeSpeed(float spd) {
+        speed = Mathf.Clamp(spd, 0, 8);
+    }
+    public void ChangeMaxForce(float mxForce) {
+        maxForce = Mathf.Clamp(mxForce, 0, 1);
+    }
+    public void ChangePerception(float prcptn) {
+        perceptionRange = Mathf.Clamp(prcptn, 0, 2);
+    }
+    public void ChangeSeparation(float seprtn) {
+        separationStrength = Mathf.Clamp(seprtn, 0, 1);
+    }
+    public void ChangeAlignment(float alignmt) {
+        alignmentStrength = Mathf.Clamp(alignmt, 0, 1);
+    }
+    public void ChangeCohesion(float cohesn) {
+        cohesionStrength = Mathf.Clamp(cohesn, 0, 1);
+    }
+    public void ToggleDebugLines() {
+        drawDebugLines = !drawDebugLines;
+    }
 }
